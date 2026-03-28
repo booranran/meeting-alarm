a407f579bb956c2a6fcc6dcd89fd787e7216035a
booranran <bbbboran@naver.com>
2026-03-28 14:26:39 +0900

    Initial Commit

---
+++ MeetingAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetingAlarm
+//
+//  Created by 최보란 on 3/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetingAlarm/MeetingAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAlarmApp.swift
+//  MeetingAlarm
+//
+//  Created by 최보란 on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetingAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
